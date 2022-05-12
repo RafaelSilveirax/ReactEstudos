4b249e4523cd449af6496e7495e1d10ee92dd79f
Rafael Silveira <rafaelsilveira2011@live.com>
2022-05-12 10:32:49 -0300

    aula lista com map

--- meu-projeto/src/App.jsx
+++ meu-projeto/src/App.jsx
@@ -1,44 +1,17 @@
 import './App.css';
-/* import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List'; */
-/* import Evento from './components/Evento';
-import Form from './components/Form'; */
+
 import Condicional from './components/Condicional';
+import { OutraLista } from './components/OutraLista';
 
 function App() {
 
-/*   const name = "Rafael";
-  const nomeEx = "shalala"
-
-  function sum(a, b){
-    return a + b;
-  }
-
-  const url = "https://via.placeholder.com/150"; */
+  const meusItens = ['React', 'Vue', 'Angular',]
 
   return (
     <div className="App">
       <h1>Renderização Condicional</h1>
-      <Condicional/>
-     
-
-      
-    {/*   <p>Primeiro projeto</p>
-      <Frase/>
-      <p>Olá, {name}</p>
-      <p>Soma: {sum(2,2)}</p>
-      <img src={url} alt=""></img>
-      <HelloWorld />
-      <SayMyName nome="XXX" />
-      <SayMyName nome={nomeEx} />
-      <Pessoa nome="uiui" idade="99" foto={url} />
-      <List/>
-      <h1>Olá Mundo React</h1>
-      <h1>Olá Mundo React</h1>
-      <h1>Olá Mundo React</h1> */}
+      <OutraLista itens={meusItens}/>
+      <OutraLista itens={[]}/>
     </div>
   );
 }
